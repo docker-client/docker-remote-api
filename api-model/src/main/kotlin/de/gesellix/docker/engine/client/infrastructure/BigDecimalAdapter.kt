f78ea5494f72de1c54bd7289a497fb29bc05d008
Tobias Gesellchen <tobias@gesellix.de>
2021-10-03 17:35:54 +0200

    generate sources for the v1.41 api

---
+++ api-model/src/main/kotlin/de/gesellix/docker/engine/client/infrastructure/BigDecimalAdapter.kt
@@ -0,0 +1,17 @@
+package de.gesellix.docker.engine.client.infrastructure
+
+import com.squareup.moshi.FromJson
+import com.squareup.moshi.ToJson
+import java.math.BigDecimal
+
+class BigDecimalAdapter {
+  @ToJson
+  fun toJson(value: BigDecimal): String {
+    return value.toPlainString()
+  }
+
+  @FromJson
+  fun fromJson(value: String): BigDecimal {
+    return BigDecimal(value)
+  }
+}
