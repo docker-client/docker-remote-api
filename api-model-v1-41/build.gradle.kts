4aea4cf7e2a9764925d2b957e85098e6805b0ac9
Tobias Gesellchen <tobias@gesellix.de>
2022-02-26 21:20:28 +0100

    Add a minimum of tests

--- api-model-v1-41/build.gradle.kts
+++ api-model-v1-41/build.gradle.kts
@@ -71,11 +71,21 @@ dependencies {
 
   implementation("com.squareup.moshi:moshi:1.13.0")
   ksp("com.squareup.moshi:moshi-kotlin-codegen:1.13.0")
+
+  testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+  testRuntimeOnly("cglib:cglib-nodep:3.3.0")
+  testImplementation("org.junit.platform:junit-platform-launcher:1.8.2")
+  testImplementation("org.junit.platform:junit-platform-commons:1.8.2")
 }
 
 java {
   sourceCompatibility = JavaVersion.VERSION_1_8
   targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.withType(Test::class) {
+  useJUnitPlatform()
 }
 
 val javadocJar by tasks.registering(Jar::class) {
