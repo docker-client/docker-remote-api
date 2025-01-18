ed339a8cb43333d44ff44e3152ef1104c7fbe213
Tobias Gesellchen <tobias@gesellix.de>
2025-01-18 20:56:27 +0100

    Migrate to Gradle's Version Catalogs (#403)
    https://docs.gradle.org/current/userguide/version_catalogs.html

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,25 +4,25 @@ import java.util.*
 rootProject.extra.set("artifactVersion", SimpleDateFormat("yyyy-MM-dd\'T\'HH-mm-ss").format(Date()))
 
 plugins {
-  id("maven-publish")
-  id("com.github.ben-manes.versions") version "0.51.0"
-  id("net.ossindex.audit") version "0.4.11"
-  id("io.freefair.maven-central.validate-poms") version "8.11"
-  id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
-  id("org.jetbrains.kotlin.jvm") version "2.1.0" apply false
-  id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
-  id("org.openapi.generator") version "7.10.0" apply false
-  id("org.jlleitschuh.gradle.ktlint") version "12.1.2" apply false
+    id("maven-publish")
+    id("com.github.ben-manes.versions") version "0.51.0"
+    id("net.ossindex.audit") version "0.4.11"
+    id("io.freefair.maven-central.validate-poms") version "8.11"
+    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
+    alias(libs.plugins.kotlinJvm) apply false
+    id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
+    id("org.openapi.generator") version "7.10.0" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.2" apply false
 }
 
 val dependencyVersions = listOf(
-  "org.jetbrains.kotlin:kotlin-reflect:2.1.0",
-  "org.jetbrains.kotlin:kotlin-script-runtime:2.1.0",
-  "org.jetbrains.kotlin:kotlin-stdlib:2.1.0",
-  "org.jetbrains.kotlin:kotlin-stdlib-common:2.1.0",
-  "org.jetbrains.kotlin:kotlin-stdlib-jdk7:2.1.0",
-  "org.jetbrains.kotlin:kotlin-stdlib-jdk8:2.1.0",
-  "org.slf4j:slf4j-api:2.0.16",
+    libs.kotlinReflect,
+    libs.kotlinScriptRuntime,
+    libs.kotlinStdlib,
+    libs.kotlinCommon,
+    libs.kotlinJdk7,
+    libs.kotlinJdk8,
+    libs.slf4j,
 )
 
 val dependencyGroupVersions = mapOf<String, String>(
@@ -31,32 +31,32 @@ val dependencyGroupVersions = mapOf<String, String>(
 )
 
 subprojects {
-  configurations.all {
-    resolutionStrategy {
-      failOnVersionConflict()
-      force(dependencyVersions)
-      eachDependency {
-        val forcedVersion = dependencyGroupVersions[requested.group]
-        if (forcedVersion != null) {
-          useVersion(forcedVersion)
+    configurations.all {
+        resolutionStrategy {
+            failOnVersionConflict()
+            force(dependencyVersions)
+            eachDependency {
+                val forcedVersion = dependencyGroupVersions[requested.group]
+                if (forcedVersion != null) {
+                    useVersion(forcedVersion)
+                }
+            }
         }
-      }
     }
-  }
 }
 
 fun findProperty(s: String) = project.findProperty(s) as String?
 
 val isSnapshot = project.version == "unspecified"
 nexusPublishing {
-  repositories {
-    if (!isSnapshot) {
-      sonatype {
-        // 'sonatype' is pre-configured for Sonatype Nexus (OSSRH) which is used for The Central Repository
-        stagingProfileId.set(System.getenv("SONATYPE_STAGING_PROFILE_ID") ?: findProperty("sonatype.staging.profile.id")) //can reduce execution time by even 10 seconds
-        username.set(System.getenv("SONATYPE_USERNAME") ?: findProperty("sonatype.username"))
-        password.set(System.getenv("SONATYPE_PASSWORD") ?: findProperty("sonatype.password"))
-      }
+    repositories {
+        if (!isSnapshot) {
+            sonatype {
+                // 'sonatype' is pre-configured for Sonatype Nexus (OSSRH) which is used for The Central Repository
+                stagingProfileId.set(System.getenv("SONATYPE_STAGING_PROFILE_ID") ?: findProperty("sonatype.staging.profile.id")) //can reduce execution time by even 10 seconds
+                username.set(System.getenv("SONATYPE_USERNAME") ?: findProperty("sonatype.username"))
+                password.set(System.getenv("SONATYPE_PASSWORD") ?: findProperty("sonatype.password"))
+            }
+        }
     }
-  }
 }
