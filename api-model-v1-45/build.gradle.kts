51fee97e9b22ff2a1246c4101074a629aea45664
Tobias Gesellchen <tobias@gesellix.de>
2026-08-22 23:27:12 +0200

    use io.github.ben-manes.versions plugin id

--- api-model-v1-45/build.gradle.kts
+++ api-model-v1-45/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     id("com.google.devtools.ksp")
     id("maven-publish")
     id("signing")
-    id("com.github.ben-manes.versions")
+    id("io.github.ben-manes.versions")
     id("org.sonatype.gradle.plugins.scan")
     id("io.freefair.maven-central.validate-poms")
     id("org.openapi.generator")
